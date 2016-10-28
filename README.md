6eb67dc9e8aa87a4d9757358b81b584f1e7f1bdc
Lars Wittenberg <larswittenberg@googlemail.com>
2016-10-29 00:36:15 +0200

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Frontend Starterkit
+
+Grundgerüst für Frontend-Projekte mit Browsersync, SASS, jQuery und dem wegmeister Grid.
---
+++ gulpfile.js
@@ -0,0 +1,34 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync').create();
+var sass        = require('gulp-sass');
+var sourcemaps  = require('gulp-sourcemaps');
+
+
+//
+// Static Server + watching scss/html files
+//
+gulp.task('serve', ['sass'], function() {
+
+  browserSync.init({
+      server: "./app"
+  });
+
+  gulp.watch("app/scss/*.scss", ['sass']);
+  gulp.watch("app/*.html").on('change', browserSync.reload);
+});
+
+
+
+//
+// Compile Sass into CSS & auto-inject into browsers
+//
+gulp.task('sass', function() {
+  return gulp.src("app/scss/*.scss")
+    .pipe(sourcemaps.init())
+    .pipe(sass())
+    .pipe(sourcemaps.write('./'))
+    .pipe(gulp.dest("app/css"))
+    .pipe(browserSync.stream());
+});
+
+gulp.task('default', ['serve']);
